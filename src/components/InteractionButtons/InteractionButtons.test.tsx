faa643bea7540248d627bbf2d9c2506d1cc74fc6
Marten <derapfelesser@aol.de>
2022-09-21 21:59:11 +0200

    test(interaction-buttons): onLikeChange triggers if like button is clicked

--- src/components/InteractionButtons/InteractionButtons.test.tsx
+++ src/components/InteractionButtons/InteractionButtons.test.tsx
@@ -1,1 +1,18 @@
-export {};
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import InteractionButtons from './InteractionButtons';
+
+test.todo('provided username links to profile');
+
+test('onLikeChange triggers if like button is clicked', async () => {
+  const mockFunction = jest.fn();
+  render(<InteractionButtons onLikeChange={mockFunction} isLiked />);
+  const user = userEvent.setup();
+  const likeButton = screen.getByRole('button', { name: 'Like' });
+
+  await user.click(likeButton);
+
+  expect(mockFunction).toHaveBeenCalled();
+});
+
+test.todo('if liked heart icon is red');
