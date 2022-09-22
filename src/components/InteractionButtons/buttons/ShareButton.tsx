6e394e0079111f64e75823498d28fc838c5ae38c
Marten <derapfelesser@aol.de>
2022-09-22 13:40:03 +0200

    feat(share-button): copy content to clipboard if clicked

--- src/components/InteractionButtons/buttons/ShareButton.tsx
+++ src/components/InteractionButtons/buttons/ShareButton.tsx
@@ -4,8 +4,15 @@ import IconContainer from './IconContainer';
 import ShareIcon from '../assets/chainSVG.js';
 
 function ShareButton(): JSX.Element {
+  async function onClick(): Promise<void> {
+    if (navigator.clipboard) {
+      const currentHREF = window.location.href;
+      await navigator.clipboard.writeText(currentHREF);
+    }
+  }
+
   return (
-    <Button aria-label="Copy link">
+    <Button aria-label="Copy link" {...{ onClick }}>
       <IconContainer iconColor="white">
         <ShareIcon />
       </IconContainer>
