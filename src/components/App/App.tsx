312a4b9ae03bc71915e355ab4320570e30949741
Marten <derapfelesser@aol.de>
2022-09-25 13:00:50 +0200

    feat(app): move app content in container

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,24 @@
+import styled from 'styled-components';
 import InteractionButtons from '../InteractionButtons';
 
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  max-width: 16rem;
+  
+  margin: 0 auto;
+`;
+
 function App(): JSX.Element {
-  return <InteractionButtons />;
+  return (
+    <AppContainer>
+      <InteractionButtons
+        onLikeChange={() => undefined}
+        isLiked
+        onCommentClick={() => undefined}
+      />
+    </AppContainer>
+  );
 }
 
 export default App;
