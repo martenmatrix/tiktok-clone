d619e9febad7c2c9eb974ede99ceb41b19bac706
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2023-02-19 11:01:23 +0100

    feat(login-modal): import logos for services and create basic container for them

--- src/components/LoginModal/loginModal.tsx
+++ src/components/LoginModal/loginModal.tsx
@@ -1,4 +1,8 @@
 import styled from 'styled-components';
+import AppleLogo from './assets/apple_logo.svg';
+import GitHubLogo from './assets/github_logo.svg';
+import TwitterLogo from './assets/twitter_logo.svg';
+import GoogleLogo from './assets/google_logo.svg';
 
 type LoginModalProps = {
     isVisible: boolean;
@@ -40,6 +44,30 @@ const CloseButton = styled.div`
   right: 1rem;
 `;
 
+const LogoContainer = styled.div`
+  width: 100%;
+  height: 10rem;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+const Logo = styled.img`
+  display: block;
+  width: 5rem;
+  height: 5rem;
+`;
+
+function LoginButtons(): JSX.Element {
+  const logos: string[] = [AppleLogo, GoogleLogo, TwitterLogo, GitHubLogo];
+
+  return (
+    <LogoContainer>
+      {logos.map((logo) => <Logo src={logo} />)}
+    </LogoContainer>
+  );
+}
+
 function LoginModal({
   isVisible,
   onClose,
@@ -51,6 +79,7 @@ function LoginModal({
     <Container>
       <Modal>
         <CloseButton>+</CloseButton>
+        <LoginButtons />
       </Modal>
     </Container>
   );
