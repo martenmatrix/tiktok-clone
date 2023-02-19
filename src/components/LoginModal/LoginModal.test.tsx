8f7a9b6fc6689e40a4ae641a2dd65ae527fef84c
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2023-02-19 21:07:12 +0100

    test(login-modal): test if isVisible property hides or shows dialog

--- src/components/LoginModal/LoginModal.test.tsx
+++ src/components/LoginModal/LoginModal.test.tsx
@@ -1,27 +1,42 @@
+/* eslint-disable */
+// @ts-nocheck
+
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import LoginModal from './LoginModal';
 
-isVisible, 
-                    onClose,
-                    onGitHubLogin,
-                    onGoogleLogin,
-                    onTwitterLogin,
-                    onAppleLogin
+const mockOnClose = jest.fn();
+const mockGoogleLogin = jest.fn();
+const mockTwitterLogin = jest.fn();
+const mockAppleLogin = jest.fn();
+const mockGitHubLogin = jest.fn();
 
-test.todo('if isVisible is true modal gets shown', () => {
-    render(<LoginModal isVisible onClose={}/>);
+test('if isVisible is true modal gets shown', () => {
+    render(<LoginModal isVisible
+        onClose={mockOnClose}
+        onGoogleLogin={mockGoogleLogin}
+        onTwitterLogin={mockTwitterLogin}
+        onAppleLogin={mockAppleLogin}
+        onGitHubLogin={mockGitHubLogin} />);
+
+    const modal = screen.getByRole('dialog');
+    expect(modal).toBeVisible();
 });
 
-test.todo('if isVisible is false modal is hidden', () => {
+test('if isVisible is false modal is hidden', () => {
+    render(<LoginModal isVisible={false}
+        onClose={mockOnClose}
+        onGoogleLogin={mockGoogleLogin}
+        onTwitterLogin={mockTwitterLogin}
+        onAppleLogin={mockAppleLogin}
+        onGitHubLogin={mockGitHubLogin} />);
 
+    const modal = screen.getByRole('dialog');
+    expect(modal).not.toBeVisible();
 });
 
-test.todo('if if close button is clicked onClose() is called', () => {
-
-});
-
+test.todo('if if close button is clicked onClose() is called');
 test.todo('if login with google is clicked onGoogleLogin gets called');
 test.todo('if login with twitter is clicked onTwitterLogin gets called');
 test.todo('if login with apple is clicked onAppleLogin gets called');
