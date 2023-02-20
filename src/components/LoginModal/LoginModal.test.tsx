c18a1f30acb80ea1ba1a85d118302e5632f3906c
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2023-02-20 11:26:42 +0100

    test(login-modal): test if onClose gets called when close button is clicked

--- src/components/LoginModal/LoginModal.test.tsx
+++ src/components/LoginModal/LoginModal.test.tsx
@@ -37,7 +37,21 @@ test('if isVisible is false modal is hidden', () => {
   expect(modal).not.toBeVisible();
 });
 
-test.todo('if if close button is clicked onClose() is called');
+test('if if close button is clicked onClose() is called', async () => {
+  const user = userEvent.setup();
+  render(<LoginModal
+    isVisible
+    onClose={mockOnClose}
+    onGoogleLogin={mockGoogleLogin}
+    onTwitterLogin={mockTwitterLogin}
+    onAppleLogin={mockAppleLogin}
+    onGitHubLogin={mockGitHubLogin}
+  />);
+
+  const closeButton = screen.getByRole('button', { name: 'close dialog' });
+  await user.click(closeButton);
+  expect(mockOnClose).toHaveBeenCalledTimes(1);
+});
 test.todo('if login with google is clicked onGoogleLogin gets called');
 test.todo('if login with twitter is clicked onTwitterLogin gets called');
 test.todo('if login with apple is clicked onAppleLogin gets called');
