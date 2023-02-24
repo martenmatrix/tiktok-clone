65ef6a14ed9fa4de240746dbcb8cca6e2c405ab5
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2023-02-24 16:06:40 +0100

    feat(feed): correct placement for interaction buttons

--- src/views/Feed/Feed.tsx
+++ src/views/Feed/Feed.tsx
@@ -1,11 +1,22 @@
 import styled from 'styled-components';
 import { useState, useCallback } from 'react';
-import { InteractionButtons } from '../../components';
+import InteractionButtons from '../../components/InteractionButtons';
 
 const FeedContainer = styled.div`
   position: relative;
   width: 100%;
   height: 100%;
+`;
+
+const VideoContainer = styled.div`
+  display: flex;
+`;
+
+const InteractionButtonsMidRight = styled(InteractionButtons)`
+  position: absolute;
+  right: 0;
+  top: 0;
+  bottom: 0;
 `;
 
 function Feed(): JSX.Element {
@@ -17,11 +28,12 @@ function Feed(): JSX.Element {
 
   return (
     <FeedContainer>
-      <InteractionButtons
+      <InteractionButtonsMidRight
         isLiked={isLiked}
         onCommentClick={() => {}}
         onLikeChange={handleLikeChange}
       />
+      <VideoContainer />
     </FeedContainer>
   );
 }
