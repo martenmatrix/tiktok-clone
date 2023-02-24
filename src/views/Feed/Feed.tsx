ea47667dd7be7b97a65affc10914ee2418455c71
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2023-02-24 15:18:50 +0100

    feat(feed): create container for feed and like state

--- src/views/Feed/Feed.tsx
+++ src/views/Feed/Feed.tsx
@@ -1,5 +1,29 @@
+import styled from 'styled-components';
+import { useState, useCallback } from 'react';
+import { InteractionButtons } from '../../components';
+
+const FeedContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`;
+
 function Feed(): JSX.Element {
-  return <p>works</p>;
+  const [isLiked, setIsLiked] = useState<boolean>(false);
+
+  const handleLikeChange = useCallback(() => {
+    setIsLiked(!isLiked);
+  }, [isLiked]);
+
+  return (
+    <FeedContainer>
+      <InteractionButtons
+        isLiked={isLiked}
+        onCommentClick={() => {}}
+        onLikeChange={handleLikeChange}
+      />
+    </FeedContainer>
+  );
 }
 
 export default Feed;
