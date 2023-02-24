24cb21a7acffbd47b966fe0b85b21ed9e12ed336
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2023-02-24 11:08:18 +0100

    test(navigation-bar): call onUpload on upload btn click

--- src/components/NavigationBar/NavigationBar.test.tsx
+++ src/components/NavigationBar/NavigationBar.test.tsx
@@ -1,1 +1,16 @@
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
+import NavigationBar from './NavigationBar';
+
+test('onUpload gets called when upload button is clicked', async () => {
+  const mockOnUpload = jest.fn();
+  const user = userEvent.setup();
+
+  render(<NavigationBar onUpload={mockOnUpload} />);
+  const uploadButton = screen.getByRole('button', { name: 'upload video' });
+  user.click(uploadButton);
+
+  expect(mockOnUpload).toHaveBeenCalledTimes(1);
+});
+
 export {};
