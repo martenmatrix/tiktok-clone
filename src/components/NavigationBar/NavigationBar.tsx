bc179f2315b1a44900f7a3fb36e034d2bd2f4aa5
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2023-02-24 10:29:18 +0100

    style(navigation-bar): background and placement of children

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -5,9 +5,13 @@ const NavigationBarContainer = styled.div`
   bottom: 0;
   left: 0;
   
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   width: 100%;
-  height: 1rem;
-  border: solid 1px black;
+  height: 4.75rem;
+  border-radius: 0.625rem 0.625rem 0 0;
+  background: #87878750;
 `;
 
 function NavigationBar(): JSX.Element {
