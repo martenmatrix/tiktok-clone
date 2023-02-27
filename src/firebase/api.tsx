8cc1445339ba861518fcb953a42ddc4f33b57cac
Marten <derapfelesser@aol.de>
2023-02-27 15:26:42 +0100

    fix(api): create basic structure for endpoints

---
+++ src/firebase/api.tsx
@@ -0,0 +1,9 @@
+function fetchVideo(id: number): string {
+  return 'ERROR';
+}
+
+function fetchVideoLikeStatus(id: number): boolean {
+  return false;
+}
+
+export { fetchVideo, fetchVideoLikeStatus };
