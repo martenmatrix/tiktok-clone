11f685fa6d4925260241d90050c60c4fd507bbcc
Marten <derapfelesser@aol.de>
2023-03-24 12:33:56 +0100

    feat(firebase): convert assets to base64 strings and use them as blobs
    Previously these files could not be uploaded to the dev database, as webpack only stores the file path in the "import"-variable.

--- src/firebase/createDevEnvironment.ts
+++ src/firebase/createDevEnvironment.ts
@@ -3,9 +3,9 @@
  * so that the application can be used without
  * manipulating the real database.
  */
-import sample1 from './assets/sample1.base64.txt';
-import sample2 from './assets/sample2.mp4';
-import sample3 from './assets/sample3.mp4';
+import sample1 from './assets/sample1.base64.js';
+import sample2 from './assets/sample2.base64.js';
+import sample3 from './assets/sample3.base64.js';
 import { loginWithMail, registerWithMail } from './login';
 import { uploadVideo } from './api';
 
@@ -19,9 +19,18 @@ async function createFakeUsers(): Promise<void> {
 }
 
 async function uploadSampleVideos(): Promise<void> {
-  const videoBlob1 = new File([sample1], 'sample1');
-  const videoBlob2 = new Blob([sample2], { type: 'video/mp4' });
-  const videoBlob3 = new Blob([sample3], { type: 'video/mp4' });
+  const sample1Res = await fetch(sample1);
+  const sample1Blob = await sample1Res.blob();
+
+  const sample2Res = await fetch(sample2);
+  const sample2Blob = await sample2Res.blob();
+
+  const sample3Res = await fetch(sample3);
+  const sample3Blob = await sample3Res.blob();
+
+  const videoBlob1 = new Blob([sample1Blob], { type: 'video/mp4' });
+  const videoBlob2 = new Blob([sample2Blob], { type: 'video/mp4' });
+  const videoBlob3 = new Blob([sample3Blob], { type: 'video/mp4' });
 
   await uploadVideo(videoBlob1);
   await uploadVideo(videoBlob2);
