0076b2a7444e4215f52bc660fe4d4dc8c86befbf
Marten <derapfelesser@aol.de>
2023-03-31 21:48:40 +0200

    feat(feed): create scrollable feed

--- src/views/Feed/Feed.tsx
+++ src/views/Feed/Feed.tsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import Video from '../../components/Video';
+import { getAllVideoIds } from '../../firebase/api';
+
+const FeedContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  overflow-y: scroll;
+  cursor: grab;
+`;
+
 function Feed(): JSX.Element {
-  return <p>works</p>;
+  const [ids, setIds] = useState<string[]>([]);
+
+  useEffect(() => {
+    async function getIds(): Promise<string[]> {
+      return getAllVideoIds();
+    }
+    getIds().then((res) => setIds(res));
+  }, []);
+
+  return (
+    <FeedContainer>
+      {ids.map((id) => <Video id={id} key={id} />)}
+    </FeedContainer>
+  );
 }
 
 export default Feed;
