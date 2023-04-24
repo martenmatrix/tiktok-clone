b3e31a78696995387b4ec6035794f9959db99044
Marten <derapfelesser@aol.de>
2023-04-24 11:18:21 +0200

    feat(app): execute firebase on page load

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './components';
 import GlobalStyle from './globalStyle';
+import { app } from './firebase/firebaseApp.js';
+
+console.log(app);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
