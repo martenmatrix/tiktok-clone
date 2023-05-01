be799a04ad0bf2ca4c91c11f2783ec1bfe20a902
Marten <derapfelesser@aol.de>
2023-05-01 21:29:13 +0200

    test(usersettings): describe tests to be implemented

--- src/views/UserSettings/UserSettings.test.tsx
+++ src/views/UserSettings/UserSettings.test.tsx
@@ -1,1 +1,19 @@
-export {};
+import { render, screen } from '@testing-library/react';
+import { getUsername, setUsername } from '../../firebase/api';
+import UserSettings from './UserSettings';
+
+const mockGetUsername = getUsername as jest.MockedFunction<typeof getUsername>;
+const mockSetUsername = setUsername as jest.MockedFunction<typeof setUsername>;
+
+test('fetches username and displays it', () => {
+  mockGetUsername.mockResolvedValue('peter');
+  render(<UserSettings />);
+});
+
+test('automatically updates username when typing in new username', () => {
+
+});
+
+test('displays mail and unable to edit input field', () => {
+
+});
