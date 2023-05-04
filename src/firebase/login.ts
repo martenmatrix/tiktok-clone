e191e0504728c84aba644ef91f27015e3df5fd93
Marten <derapfelesser@aol.de>
2023-05-04 21:51:54 +0200

    feat(register): adds mail to db entry when user creates acc

--- src/firebase/login.ts
+++ src/firebase/login.ts
@@ -5,18 +5,18 @@ import {
 import { getDoc, setDoc, doc } from 'firebase/firestore';
 import { auth, db } from './firebaseApp.js';
 
-async function createUserDBEntry(userId: string): Promise<void> {
+async function createUserDBEntry(userId: string, mail: string): Promise<void> {
   const userDoc = doc(db, 'users', userId);
   const userSnap = await getDoc(userDoc);
 
   if (!userSnap.exists()) {
-    await setDoc(userDoc, { profilePicture: 'undefined', username: userId });
+    await setDoc(userDoc, { profilePicture: 'undefined', username: userId, mail });
   }
 }
 
 async function registerWithMail(mail: string, password: string): Promise<void> {
   const response = await createUserWithEmailAndPassword(auth, mail, password);
-  await createUserDBEntry(response.user.uid);
+  await createUserDBEntry(response.user.uid, mail);
 }
 async function loginWithMail(mail: string, password: string) {
   await signInWithEmailAndPassword(auth, mail, password);
