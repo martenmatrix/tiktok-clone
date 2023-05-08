6f41f73880ef43abf86e753252da3229d52048fe
Marten <derapfelesser@aol.de>
2023-05-08 20:39:47 +0200

    refactor(user-settings): move input component to own file

---
+++ src/components/Input.tsx
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+import React from 'react';
+
+const StyledLabel = styled.label`
+  font-size: 15px;
+`;
+
+const StyledInput = styled.input`
+  border: solid 1px #00000030;
+  font-size: 20px;
+  color: #000000;
+  height: 40px;
+  border-radius: 5px;
+  cursor: ${(props) => (props.disabled ? 'not-allowed' : 'pointer')};
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-family: 'Montserrat', sans-serif;
+  color: #00000050;
+`;
+
+type InputType = {
+  label: string,
+  // eslint-disable-next-line no-unused-vars
+  onChange?: (e: any) => void,
+  value: string,
+  disabled?: boolean,
+}
+
+function Input({
+  onChange = (() => {}), value, label, disabled = false,
+}: InputType): JSX.Element {
+  return (
+    <InputContainer>
+      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+      <StyledLabel htmlFor={label}>{label}</StyledLabel>
+      <StyledInput onChange={onChange} value={value} name="username" id={label} type="text" disabled={disabled} />
+    </InputContainer>
+  );
+}
+
+export default Input;
--- src/views/UserSettings/UserSettings.tsx
+++ src/views/UserSettings/UserSettings.tsx
@@ -1,46 +1,7 @@
 import React, { useState, useCallback } from 'react';
 import styled from 'styled-components';
+import Input from '../../components/Input';
 import { getUsername, setUsername } from '../../firebase/api';
-
-const StyledInput = styled.input`
-  border: solid 1px #00000030;
-  font-size: 20px;
-  color: #000000;
-  height: 40px;
-  border-radius: 5px;
-  cursor: ${(props) => (props.disabled ? 'not-allowed' : 'pointer')};
-`;
-
-const StyledLabel = styled.label`
-  font-size: 15px;
-`;
-
-const InputContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  font-family: 'Montserrat', sans-serif;
-  color: #00000050;
-`;
-
-type InputType = {
-  label: string,
-  // eslint-disable-next-line no-unused-vars
-  onChange?: (e: any) => void,
-  value: string,
-  disabled?: boolean,
-}
-
-function Input({
-  onChange = (() => {}), value, label, disabled = false,
-}: InputType): JSX.Element {
-  return (
-    <InputContainer>
-      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-      <StyledLabel htmlFor={label}>{label}</StyledLabel>
-      <StyledInput onChange={onChange} value={value} name="username" id={label} type="text" disabled={disabled} />
-    </InputContainer>
-  );
-}
 
 const UserSettingsContainer = styled.div`
   display: flex;
