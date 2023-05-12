a460d1cb11bbf8ed0abd4fced4f0c5177bc0b63a
Marten <derapfelesser@aol.de>
2023-05-12 20:39:13 +0200

    test(login modal): add tests to implement

--- src/components/LoginModal/LoginModal.test.tsx
+++ src/components/LoginModal/LoginModal.test.tsx
@@ -42,3 +42,9 @@ test('if if close button is clicked onClose() is called', async () => {
   await user.click(closeButton);
   expect(mockOnClose).toHaveBeenCalledTimes(1);
 });
+
+test.todo('if loginWithMail succeeds calls onSuccess and does not call registerWithMail');
+test.todo('if loginWithMail does not succeed calls registerWithMail');
+test.todo('if loginWithMail and registerWithMail does not succeed do not call registerWithMail');
+test.todo('does not execute if mail is empty');
+test.todo('does not execute if password is less than 6 characters');
