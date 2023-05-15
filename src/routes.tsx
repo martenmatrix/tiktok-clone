f447fdc7bdfeb0692d913e222a9074f6bc3c1624
Marten <derapfelesser@aol.de>
2023-05-15 20:27:12 +0200

    feat(routes): define routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Navigate, Route } from 'react-router-dom';
 import { App } from './components';
 import { Feed, UserSettings } from './views';
 
-const AvailableRoutes: JSX.Element = (
-  <Routes>
-    <Route path="/" element={<App />}>
-      <Route path="feed" element={<Feed />} />
-      <Route path="userSettings" element={<UserSettings />} />
-    </Route>
-  </Routes>
-);
+function AvailableRoutes(): JSX.Element {
+  return (
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Navigate to="/feed" />} />
+        <Route index path="feed" element={<Feed />} />
+        <Route path="userSettings" element={<UserSettings />} />
+      </Route>
+    </Routes>
+  );
+}
 
 export default AvailableRoutes;
