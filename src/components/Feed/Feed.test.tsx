9994f4c97a9cf879ee1b04b72b1b8ccb0d9ddeb9
Marten <derapfelesser@aol.de>
2023-05-16 11:26:37 +0200

    feat(feed): delete unused tests

--- src/components/Feed/Feed.test.tsx
+++
@@ -1,7 +0,0 @@
-test.todo(' arrow down shows next video, arrow up prev video');
-test.todo('arrow up pressed at beginning does not change video');
-test.todo('fetches videos from api and renders multiple video components');
-test.todo('id in url represents currently playing video url');
-test.todo('if called with id in url, displays video with the corresponding id');
-
-export {};
