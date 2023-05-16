aa57ff2098e2112689622d4209e50ec02cac3b11
Marten <derapfelesser@aol.de>
2023-05-16 21:11:11 +0200

    feat(video): add index export

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,8 @@ import App from './App';
 import InteractionButtons from './InteractionButtons';
 import LoginModal from './LoginModal';
 import NavigationBar from './NavigationBar';
+import Video from './Video';
 
 export {
-  App, InteractionButtons, LoginModal, NavigationBar,
+  App, InteractionButtons, LoginModal, NavigationBar, Video,
 };
