bf2acdf01545d17c56eb905515ce2d295a1ac3c3
Marten <derapfelesser@aol.de>
2023-05-16 21:30:58 +0200

    feat(firebase): disable firebase warning banner

--- src/firebase/firebaseApp.js
+++ src/firebase/firebaseApp.js
@@ -21,7 +21,7 @@ const db = getFirestore(app);
 const storage = getStorage(app);
 
 if (inDevEnvironment()) {
-  connectAuthEmulator(auth, 'http://localhost:9099');
+  connectAuthEmulator(auth, 'http://localhost:9099', { disableWarnings: true });
   connectStorageEmulator(storage, 'localhost', 9199);
   connectFirestoreEmulator(db, 'localhost', 8080);
   createDevEnvironment().catch((e) => console.warn(`Unable to create dev environment: ${e}.`));
