071b739dc323967ac66400aefabc730a2d37f62b
Marten <derapfelesser@aol.de>
2023-05-17 21:52:08 +0200

    feat(index): create dev environment if in dev env

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import inDevEnvironment from './firebase/inDevEnvironment';
+import createDevEnvironment from './firebase/createDevEnvironment';
 import GlobalStyle from './globalStyle';
 import AvailableRoutes from './routes';
+
+inDevEnvironment() && createDevEnvironment();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
