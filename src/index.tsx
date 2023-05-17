c31442cec7e7d4b9d62c971dc332eaa7c1109b39
Marten <derapfelesser@aol.de>
2023-05-17 21:50:55 +0200

    fix(index): remove import which was only needed to prevent circular dependency error

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { app } from './firebase/firebaseApp.js';
 import GlobalStyle from './globalStyle';
 import AvailableRoutes from './routes';
-
-// This logging needs to happen, otherwise app, auth, db, storage are undefined in api functions.
-console.log(app);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
