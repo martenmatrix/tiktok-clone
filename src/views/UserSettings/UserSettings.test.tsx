183affaab8259349ce90346556ba65b844d2bcbc
Marten <derapfelesser@aol.de>
2023-05-17 20:29:10 +0200

    test(user-settings): username and mail get fetched

--- src/views/UserSettings/UserSettings.test.tsx
+++ src/views/UserSettings/UserSettings.test.tsx
@@ -1,13 +1,32 @@
-import { render, screen } from '@testing-library/react';
-import { getUsername, setUsername } from '../../firebase/api';
+import { render, screen, waitFor } from '@testing-library/react';
+import { getUsername, getMail, setUsername } from '../../firebase/api';
 import UserSettings from './UserSettings';
 
+jest.mock('../../firebase/api');
+
 const mockGetUsername = getUsername as jest.MockedFunction<typeof getUsername>;
+const mockGetMail = getMail as jest.MockedFunction<typeof getMail>;
 const mockSetUsername = setUsername as jest.MockedFunction<typeof setUsername>;
 
-test('fetches username and displays it', () => {
+beforeEach(() => {
   mockGetUsername.mockResolvedValue('peter');
+  mockGetMail.mockResolvedValue('mail@mail.mail');
+});
+
+afterEach(() => {
+  mockGetUsername.mockClear();
+  mockGetMail.mockClear();
+});
+
+test('fetches username and mail and displays it', async () => {
   render(<UserSettings />);
+  const usernameInput: HTMLInputElement = screen.getByLabelText('Username');
+  const mailInput: HTMLInputElement = screen.getByLabelText('Mail');
+
+  await waitFor(() => {
+    expect(usernameInput.value).toBe('peter');
+    expect(mailInput.value).toBe('mail@mail.mail');
+  });
 });
 
 test('automatically updates username when typing in new username', () => {
