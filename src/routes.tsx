164f148883fc90c9ec92a945727afdecd330a7b2
Marten <derapfelesser@aol.de>
2023-06-05 20:44:25 +0200

    feat(routes): use new promise api for isLoggedIn

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,7 @@
 import {
   Routes, Navigate, Route, useNavigate,
 } from 'react-router-dom';
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { isLoggedIn } from './firebase/api';
 import { App, LoginModal } from './components';
 import { Feed, UserSettings } from './views';
@@ -9,6 +9,7 @@ import { Feed, UserSettings } from './views';
 function AvailableRoutes(): JSX.Element {
   const navigate = useNavigate();
   const [showLoginModal, setShowLoginModal] = useState<boolean>(false);
+  const [authenticated, setAuthenticated] = useState<boolean>(false);
 
   const displayLoginModal = useCallback(() => {
     setShowLoginModal(true);
@@ -16,6 +17,10 @@ function AvailableRoutes(): JSX.Element {
 
   const closeLoginModal = useCallback(() => {
     setShowLoginModal(false);
+  }, []);
+
+  useEffect(() => {
+    isLoggedIn().then((res) => setAuthenticated(res));
   }, []);
 
   return (
@@ -29,7 +34,7 @@ function AvailableRoutes(): JSX.Element {
         <Route path="/" element={<App />}>
           <Route index element={<Navigate to="/feed" />} />
           <Route index path="feed" element={<Feed onActionWhichRequiresAuth={displayLoginModal} />} />
-          <Route path="userSettings" element={isLoggedIn() ? <UserSettings /> : <LoginModal isVisible onClose={() => navigate('/')} onSuccess={closeLoginModal} />} />
+          <Route path="userSettings" element={authenticated ? <UserSettings /> : <LoginModal isVisible onClose={() => navigate('/')} onSuccess={closeLoginModal} />} />
         </Route>
       </Routes>
     </>
