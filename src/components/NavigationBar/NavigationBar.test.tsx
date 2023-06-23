3d30c4d5a9af2ab6d6aa73a23d6d47288d7e5aa6
Marten <derapfelesser@aol.de>
2023-06-23 22:00:09 +0200

    test(navigation-bar): remove onClick test and test if onActionWhichRequiresAuth works

--- src/components/NavigationBar/NavigationBar.test.tsx
+++ src/components/NavigationBar/NavigationBar.test.tsx
@@ -1,18 +1,28 @@
 import userEvent from '@testing-library/user-event';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { isLoggedIn } from '../../firebase/api';
 import NavigationBar from './NavigationBar';
 
-test('onUpload gets called when upload button is clicked', async () => {
-  const mockOnUpload = jest.fn();
+jest.mock('../../firebase/api');
+
+const mockIsLoggedIn = jest.mocked(isLoggedIn);
+
+test('if uploadButton is clicked calls onActionWhichRequiresAuth if user is not logged in', async () => {
+  mockIsLoggedIn.mockResolvedValue(false);
+  const mockOnAuth = jest.fn();
   const user = userEvent.setup();
 
-  render(<NavigationBar onUpload={mockOnUpload} />);
+  render(
+    <BrowserRouter>
+      <NavigationBar onActionWhichRequiresAuth={mockOnAuth} />
+    </BrowserRouter>,
+  );
   const uploadButton = screen.getByRole('button', { name: 'upload video' });
   await user.click(uploadButton);
 
-  expect(mockOnUpload).toHaveBeenCalledTimes(1);
+  expect(mockOnAuth).toHaveBeenCalledTimes(1);
 });
 
-test.todo('if uploadButton is clicked calls onActionWhichRequiresAuth if user is not logged in');
 test.todo('if uploadButton is clicked opens context menu if user is logged in');
 test.todo('if uploadVideo api function throws error display error with alert');
