9b19b47f0c5cb264f83a9979870edfd764503667
Marten <derapfelesser@aol.de>
2023-08-12 21:23:19 +0200

    test(login-modal): test if api functions are called with correct parameters after submit

--- src/components/LoginModal/LoginModal.test.tsx
+++ src/components/LoginModal/LoginModal.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import * as firebaseUtil from '../../firebase/login';
 import '@testing-library/jest-dom';
@@ -65,4 +65,27 @@ test('if loginWithMail does not succeed calls registerWithMail', async () => {
 
   expect(loginMailMock).toHaveBeenCalledTimes(1);
   expect(registerMailMock).toHaveBeenCalledTimes(1);
+});
+
+test('when form is submitted first calls loginWithMail with entered parameters', async () => {
+  const loginMailMock = jest.spyOn(firebaseUtil, 'loginWithMail').mockRejectedValue(true);
+  const user = userEvent.setup();
+  render(<LoginModal
+    isVisible
+    onClose={mockOnClose}
+    onSuccess={mockOnSuccess}
+  />);
+
+  const mailInput = screen.getByLabelText('Mail');
+  const passwordInput = screen.getByLabelText('Password');
+  const submitButton = screen.getByRole('button');
+
+  await user.type(mailInput, 'mail@example.org');
+  await user.type(passwordInput, 'goodPassword');
+
+  await act(async () => {
+    await user.click(submitButton);
+  });
+
+  expect(loginMailMock).toHaveBeenCalledWith('mail@example.org', 'goodPassword');
 });
